77dc4ba04383ab2662d53bcadea59b7643f2bbcf
jacintoluistest <luisj81@gmail.com>
2024-07-19 16:46:30 -0600

    Agregando About Modal en index

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 include 'config.php';
 $sql = "SELECT * FROM pets";
 $result = $conn->query($sql);
+// Define the path to the build number file
+$buildNumberFile = __DIR__ . '/build_number.txt';
+// Read the build number from the file
+$buildNumber = 'Unknown';
+if (file_exists($buildNumberFile)) {
+    $buildNumber = file_get_contents($buildNumberFile);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +22,36 @@ $result = $conn->query($sql);
 <body>
     <h1>Pet List from Jenkins cambio Luis Jacinto</h1>
     <a href="create.php">Add New Pet</a>
+
+    <button class="btn btn-success btn-lg" data-toggle="modal" data-target="#modalForm">
+    Open Contact Form
+</button>
+
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Acerca de</h4>
+        </div>
+        <div class="modal-body">
+          <p>Sistema de Catálogo de Mascotas</p>
+          <p>Version<?php echo htmlspecialchars($buildNumber); ?></p>
+          <p></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+
+
+
     <table border="1">
         <tr>
             <th>ID</th>
